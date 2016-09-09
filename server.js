da2e4ec3392a70dbaac3e101bdcbf97809dc38ba
Viren Bhagat <virenb@gmail.com>
2016-09-08 21:34:00 -0400

    Added html chat, server using socket.io

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('Server Running');
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+
+});
+
+io.sockets.on('connection', function(socket) {
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
+
+	//Disconnect
+	connections.splice(connections.indexOf(socket, 1));
+	console.log('Disconnected: %s sockets connected', connections.length);
+});
